4857412bbeb46109653711c2ed161f0f4b5783ae
Thiago Rosa <thiagorcon@hotmail.com>
2024-07-05 21:01:13 -0300

    Tarefa 4 concluída

---
+++ src/script/index.js
@@ -0,0 +1,33 @@
+function modal() {
+  const modalContainer = document.querySelector("#modalContainer");
+  const bnt = document.querySelector("#showMessage");
+
+  bnt.addEventListener("click", () => {
+    modalContainer.showModal();
+  });
+
+  closeModal();
+}
+
+function modal2() {
+  const modalContainer = document.querySelector("#modalContainer");
+  const bnt = document.querySelector("#showMessage2");
+
+  bnt.addEventListener("click", () => {
+    modalContainer.showModal();
+  });
+
+  closeModal();
+}
+
+function closeModal() {
+  
+  btnClose = document.querySelector("#closeModal");
+
+  btnClose.addEventListener("click", () => {
+    modalContainer.close();
+  });
+}
+
+modal();
+modal2();
